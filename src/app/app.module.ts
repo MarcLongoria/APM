557291a68a0e44cd301920ff43b7def7ec3378e5
Marc Longoria <marclongoria@outlook.com>
2018-05-11 14:49:45 -0500

    adding stars component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,12 @@ import { FormsModule} from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './products/product-list.component';
+import { StarComponent } from './shared/star.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent, ProductListComponent
+    AppComponent, ProductListComponent, StarComponent
   ],
   imports: [
     BrowserModule, FormsModule
