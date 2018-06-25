e2f44ced5f19c4bb3af20bcbf8fdde5bc06a182e
Marc Longoria <marclongoria@outlook.com>
2018-06-24 23:40:21 -0500

    routing section

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule} from '@angular/forms';
+import { RouterModule} from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './products/product-list.component';
@@ -18,7 +19,8 @@ import { WelcomeComponent } from './home/welcome.component';
     AppComponent, ProductListComponent, StarComponent, ConvertToSpace, ProductDetailComponent, WelcomeComponent
   ],
   imports: [
-    BrowserModule, FormsModule, HttpClientModule
+    BrowserModule, FormsModule, HttpClientModule, RouterModule.forRoot([{path: 'products', component: ProductListComponent}, {path: 'products/:id', component: ProductDetailComponent},
+  {path: 'welcome', component: WelcomeComponent},{path: '', redirectTo: 'welcome', pathMatch: 'full'},{path: '**', redirectTo: 'welcome', pathMatch: 'full'}])
   ],
   providers: [ProductService],
   bootstrap: [AppComponent]
